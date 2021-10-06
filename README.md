fc5ea5269c6e7d986a7e1b13a42064eb3391ce9a
galaxine <oezdemir50@gmail.com>
2021-10-06 14:29:49 +0200

    The boxes work now as intended

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # etch-a-sketch
 
-
 ## What is this?
+
 Another project where I am attempting to apply what I learned into a project for my portfolio.
 The underlying theme here is DOM-Manipulation
 
+## What do I have problems with?
+
+I still have a disconnect between 2d-arrays and nodelist.
+I created these divs through javascript to save me time, but I am having troubles seeing the **Distinct** difference here.
+
+What's not shown is, that I am working with Typescript behind the scenes, the code is transpiled into es6 and I haven't much used typescript except for the typing and showing me how I should work wit ambigious types like this:
+`let box = event.target as HTMLSelectElement;`.
+It's quite helpful as it helps me think more about the types I am using and reduces uncertainty for a beginner.
--- app.js
+++ app.js
@@ -1,28 +1,40 @@
+//fist of all, declare the 2d-array we are using to "visually" show the change later
+//nothing to do with the browser, arrays and nodelists are different
+let size = 1024;
+let divContainer = [];
+let containerSize = 400;
 // create a container div after getting the body
 const body = document.querySelector("body");
 const container = document.createElement("div");
 container.className = "div-container";
 container.setAttribute("style", `
   display: grid;
-  grid-template-columns: repeat(16,1fr);
-  width: 400px;
-  height: 400px;
-  gap: 5px;
- `);
+  grid-template-columns: repeat(${size},1fr);
+  width: ${containerSize}px;
+  height: ${containerSize}px;
+  `);
 if (body !== null) {
     body.appendChild(container);
 }
-let size = 16;
-let divContainer = [];
 for (let i = 0; i < size; i++) {
     let divContainer2 = [];
     for (let j = 0; j < size; j++) {
         const divNode = document.createElement("div");
         divNode.className = "boxes";
-        divNode.setAttribute("style", `background-color: black`);
+        divNode.setAttribute("style", `
+      background-color: black;
+      width:calc(100%/${size})px;
+      height:calc(100%/${size})px;`);
         container.appendChild(divNode);
         divContainer2.push(divNode);
     }
     divContainer.push(divContainer2);
 }
-//window.addEventListener('mousemove',(event) => );
+//first add an hover effect to EVERY boxes
+//if a mouse goes over it, then they change colors
+container.onmousemove = function(event) {
+    let box = event.target;
+    if (box.className === "boxes") {
+        box.setAttribute("style", "background-color:white;");
+    }
+};
