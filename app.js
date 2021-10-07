12a6a18014d1ca78cc2410e6ba79ab3776e0fe74
galaxine <oezdemir50@gmail.com>
2021-10-07 14:27:10 +0200

    Finished the implementation. NExt feature: rainbow colors

--- app.js
+++ app.js
@@ -1,40 +1,78 @@
+// create the body
+const body = document.querySelector("body");
+//---------------------------- buttons and possibly reset function ----------
+const button = document.createElement("button");
+button.textContent = "Clear the screen?";
+button.setAttribute("style", `
+  padding: = 0 15px;
+  `);
+body.appendChild(button);
 //fist of all, declare the 2d-array we are using to "visually" show the change later
 //nothing to do with the browser, arrays and nodelists are different
-let size = 1024;
+let size = 3;
 let divContainer = [];
 let containerSize = 400;
 // create a container div after getting the body
-const body = document.querySelector("body");
 const container = document.createElement("div");
 container.className = "div-container";
-container.setAttribute("style", `
-  display: grid;
-  grid-template-columns: repeat(${size},1fr);
-  width: ${containerSize}px;
-  height: ${containerSize}px;
-  `);
+setBoxContainer();
 if (body !== null) {
     body.appendChild(container);
 }
-for (let i = 0; i < size; i++) {
-    let divContainer2 = [];
-    for (let j = 0; j < size; j++) {
-        const divNode = document.createElement("div");
-        divNode.className = "boxes";
-        divNode.setAttribute("style", `
+function makeBoxes() {
+    for (let i = 0; i < size; i++) {
+        let divContainer2 = [];
+        for (let j = 0; j < size; j++) {
+            const divNode = document.createElement("div");
+            divNode.className = "boxes";
+            divNode.setAttribute("style", `
       background-color: black;
-      width:calc(100%/${size})px;
-      height:calc(100%/${size})px;`);
-        container.appendChild(divNode);
-        divContainer2.push(divNode);
+      `);
+            container.appendChild(divNode);
+            divContainer2.push(divNode);
+        }
+        divContainer.push(divContainer2);
     }
-    divContainer.push(divContainer2);
 }
+makeBoxes();
 //first add an hover effect to EVERY boxes
 //if a mouse goes over it, then they change colors
-container.onmousemove = function(event) {
+container.onmousemove = function (event) {
     let box = event.target;
     if (box.className === "boxes") {
         box.setAttribute("style", "background-color:white;");
     }
 };
+button.addEventListener("click", resetCanvas);
+function resetCanvas() {
+    let children = container.getElementsByClassName("boxes");
+    for (let child of children) {
+        child.setAttribute("style", "background: black;");
+    }
+    setPixelAmount();
+}
+function setBoxContainer() {
+    container.setAttribute("style", `
+    display: grid;
+    grid-template-columns: repeat(${size},1fr);
+    grid-template-rows: repeat(${size}, 1fr);
+    width: ${containerSize}px;
+    height: ${containerSize}px;
+    `);
+}
+function setPixelAmount() {
+    let pixels = Number(prompt("Choose a size between 1 and 64"));
+    if (pixels > 0 && pixels <= 64) {
+        size = pixels;
+        while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+        }
+        // REALLY???? Apparently yes, because when you are actually setting the parent container up, it's **SET**.
+        // so you have to re-SET it again. Maybe we can turn it into a function to
+        setBoxContainer();
+        makeBoxes();
+    }
+    else {
+        alert("You entered an invalid number");
+    }
+}
